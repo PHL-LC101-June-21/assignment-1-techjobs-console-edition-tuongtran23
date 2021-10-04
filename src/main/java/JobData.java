d333ec10cee39e266a0b30fc9220e158d2030052
tuongtran23 <63479988+tuongtran23@users.noreply.github.com>
2021-10-04 10:22:40 -0400

    Solved assignment 1

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -5,10 +5,7 @@ import org.apache.commons.csv.CSVRecord;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by LaunchCode
@@ -77,9 +74,9 @@ public class JobData {
 
         for (HashMap<String, String> row : allJobs) {
 
-            String aValue = row.get(column);
+            String aValue = row.get(column).toUpperCase();
 
-            if (aValue.contains(value)) {
+            if (aValue.contains(value.toUpperCase())) {
                 jobs.add(row);
             }
         }
@@ -99,7 +96,20 @@ public class JobData {
         loadData();
 
         // TODO - implement this method
-        return null;
+
+        ArrayList<HashMap<String, String>> matchedJobs = new ArrayList<>();
+
+        for (HashMap<String, String> row : allJobs) {
+            for (String key : row.keySet()) {
+                String apartment = row.get(key).toUpperCase();
+                // if key is equal to value, return that job
+                if (apartment.contains(value.toUpperCase())) {
+                    matchedJobs.add(row);
+                    break;
+                }
+            }
+        }
+        return matchedJobs;
     }
 
     /**
